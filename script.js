d14f84e098478a2fe6e89b0ed3403ade44e9dd23
ShaunAryan28 <123452148+ShaunAryan28@users.noreply.github.com>
2023-08-19 19:07:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,32 @@ cookieElement.addEventListener('click', () => {
     localStorage.setItem('bakeryData', JSON.stringify(bakeryData));
     updateUI();
 });
+
+// Change bakery name
+changeNameBtn.addEventListener('click', () => {
+    const newName = prompt('Enter a new name for your bakery:');
+    if (newName) {
+        currentBakeryName = newName;
+        localStorage.setItem('currentBakeryName', newName);
+        updateUI();
+    }
+});
+
+
+const seeAllBakeriesBtn = document.getElementById('seeAllBakeriesBtn');
+const allBakeriesContainer = document.getElementById('allBakeries');
+const bakeryList = document.getElementById('bakeryList');
+
+// Function to display all bakeries
+function displayAllBakeries() {
+    bakeryList.innerHTML = ''; 
+
+    for (const bakeryName in bakeryData) {
+        const bakery = bakeryData[bakeryName];
+        const listItem = document.createElement('li');
+        listItem.textContent = `${bakeryName}: ${bakery.cookieCount} Cookies`;
+        bakeryList.appendChild(listItem);
+    }
+
+    allBakeriesContainer.classList.remove('hidden');
+}
