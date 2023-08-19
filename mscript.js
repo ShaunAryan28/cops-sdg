537c6b7664887457fcf1e9fa96695823297468da
ShaunAryan28 <123452148+ShaunAryan28@users.noreply.github.com>
2023-08-19 19:43:39 +0530

    Update mscript.js

--- mscript.js
+++ mscript.js
@@ -1,13 +1,17 @@
-
 const signupForm = document.getElementById('signupForm');
-
-
+const usrnm= "22145083";
+const pass= "1234";
 signupForm.addEventListener('submit', function (e) {
-    e.preventDefault(); // Prevent the default form submission behavior
+    e.preventDefault();
+    //Prevent the default form submission behavior
 
     // Get the entered username and password
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-
+    if(username===usrnm && pass===password){
     window.location.href = 'index.html';
+    }
+    else{
+        alert("WRONG PASSWORD OR USERNAME!!");
+    }
 });
